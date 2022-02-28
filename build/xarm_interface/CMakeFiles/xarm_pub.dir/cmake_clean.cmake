c0ae20c69296705b2ca05fe93ac41bae7927a37c
HyeWon33 <hjw141@naver.com>
2022-02-28 11:47:24 +0900

    .

---
+++ build/xarm_interface/CMakeFiles/xarm_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xarm_pub.dir/src/xarm_pub.cpp.o"
+  "/home/won/xarm_ws/devel/lib/xarm_interface/xarm_pub.pdb"
+  "/home/won/xarm_ws/devel/lib/xarm_interface/xarm_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xarm_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/xarm_interface/CMakeFiles/xarm_sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xarm_sub.dir/src/xarm_sub.cpp.o"
+  "/home/won/xarm_ws/devel/lib/xarm_interface/xarm_sub.pdb"
+  "/home/won/xarm_ws/devel/lib/xarm_interface/xarm_sub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xarm_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
