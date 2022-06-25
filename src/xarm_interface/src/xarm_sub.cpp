918b429df6b4c4ab96dc7779d1913ff8c426f1ec
HyeWon33 <hjw141@naver.com>
2022-06-25 15:03:30 +0900

    code ing

--- src/xarm_interface/src/xarm_sub.cpp
+++ src/xarm_interface/src/xarm_sub.cpp
@@ -244,38 +244,23 @@ int main(int argc, char **argv){
 
     cout << "go home" << endl;
     arm.setNamedTarget("home"); 
-    arm.move(); //robot move
+    arm.move(); 
     sleep(1);
 
     geometry_msgs::Pose target_pose;
-
-
-    geometry_msgs::PoseStamped currentPose; //curretnPose 에 = operator 없어서.
+    geometry_msgs::PoseStamped currentPose; 
     currentPose = arm.getCurrentPose();
-    cout << currentPose<<endl;
-
-
+ 
     target_pose.position.x = 0.207533;
     target_pose.position.y = -0.341972;
     target_pose.position.z = 0.591564;
-
     target_pose.orientation.x = 0.616216;
     target_pose.orientation.y = -0.346819;
     target_pose.orientation.z = 0.616182;
     target_pose.orientation.w = 0.3468565;
-
-    // target_pose.orientation = currentPose.pose.orientation;
-    
-    sleep(1);
-    arm.setStartStateToCurrentState();
     arm.setPoseTarget(target_pose, "link_tcp");
-
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    moveit_msgs::MotionPlanRequest response;
-
-    arm.plan(my_plan);
-    arm.execute(my_plan);
-
+    arm.move();
+    sleep(1);
 
     currentJointValue2[0] = 0.4;
     cout << "gripper open" << endl;
@@ -293,22 +278,13 @@ int main(int argc, char **argv){
     target_pose.position.x = 0.709229;
     target_pose.position.y = -0.00404761;
     target_pose.position.z = 0.151429;
-
     target_pose.orientation.x = 0.720195;
     target_pose.orientation.y = 0.0000165608;
     target_pose.orientation.z = 0.693761;
     target_pose.orientation.w = 0.00397689;
-
-    sleep(1);
-    // arm.setStartStateToCurrentState();
     arm.setPoseTarget(target_pose, "link_tcp");
-
-    // moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    // moveit_msgs::MotionPlanRequest response;
-
-    // arm.plan(my_plan);
-    // arm.execute(my_plan);
     arm.move();
+    sleep(1);
 
     currentJointValue2[0] = 0.4;
     cout << "gripper open" << endl;
@@ -316,12 +292,9 @@ int main(int argc, char **argv){
     gripper.move();
     sleep(1);
 
-
     cout << "go home" << endl;
     arm.setNamedTarget("home"); 
-    arm.move(); //robot move
-    sleep(1);
-    // arm.move();
+    arm.move(); 
     sleep(1);
 
     return 0;
